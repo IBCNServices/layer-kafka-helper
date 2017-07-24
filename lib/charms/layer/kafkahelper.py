254b4601bcc55669ed73b945fff3d828391ba2ee
sborny <sander.borny@ugent.be>
2017-07-24 14:21:56 +0200

    Fix broker functions

--- lib/charms/layer/kafkahelper.py
+++ lib/charms/layer/kafkahelper.py
@@ -1,10 +1,17 @@
-import os
 from charmhelpers.core import unitdata
 
+
 def brokers_as_string(delimiter):
-    brokers = unitdata.kv().get('kafka', [])
+    kafkas = unitdata.kv().get('kafka', [])
+    brokers = []
+    for kafka_unit in kafkas:
+        brokers.append(kafka_unit['host'] + ':' + str(kafka_unit['port']))
     return delimiter.join(brokers)
+
 
 def zookeepers_as_string(delimiter):
     zookeepers = unitdata.kv().get('zookeeper', [])
-    return delimiter.join(zookeepers)
+    brokers = []
+    for zookeeper_unit in zookeepers:
+        brokers.append(zookeeper_unit['host'] + ':' + str(zookeeper_unit['port']))
+    return delimiter.join(brokers)
